d36ddfdcd495e0167703e253ff1d97ae18f2f7cb
d11416216-bit <d11416216@gs.takming.edu.tw>
2026-03-20 12:08:12 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+public class Test
+{
+	private int Mid;
+
+	public int MyMid
+	{
+		get
+		{
+			return Mid;
+		}
+		set
+		{
+			if (value < 0 || value > 100)  // ||是或
+			{
+				Mid = 0;
+			}
+			else
+			{
+                Mid = value;
+            }
+        }
+	}
+
+	public int Price
+	{
+		get
+		{
+			return MyFinal;
+		}
+		set
+		{
+			if (value > 0 && value < 100)
+			{
+				Final = value;
+			}
+			else
+			{
+                Final = 0;
+            }
+;
+		}
+	}
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Class_0320_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std = new Test();
+            std.MyMid = 92;
+            Console.WriteLine(std.MyMid);
+
+            std.Price = 78;
+            Console.WriteLine(std.Price);
+            
+            
+            
+            //std.Price = 100;
+            //Console.WriteLine(std.Price);
+        }
+    }
+}
